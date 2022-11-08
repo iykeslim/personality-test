7f71e081f4515a6e1ac2c1289a2a128e27821128
iykeslim <iykeslim01@gmail.com>
2022-11-08 23:01:59 +0100

    adding dummy api data

---
+++ src/dummy-api/data.js
@@ -0,0 +1,100 @@
+export function getQuestions() {
+    const questions = [
+        {
+            question:
+                'I get uneasy when my space is not clean',
+            
+                ses: [
+                {
+                    response: 'Yes. that is accurately me',
+                    likelihoodscale: 2,
+                },
+                {
+                    response:
+                        'I really do not care',
+                    likelihoodscale: 7,
+                },
+                {
+                    response: 'I have not thought about it',
+                    likelihoodscale: 5,
+                },
+                {
+                    response:
+                        'Very accurate',
+                    likelihoodscale: 2,
+                },
+            ],
+        },
+        {
+            question:
+                'when I am invited for a party I get excited before it is time',
+            responses: [
+                {
+                    response: 'Not me at all',
+                    likelihoodscale: 3,
+                },
+                {
+                    response: 'most likely me',
+                    likelihoodscale: 6,
+                },
+                {
+                    response:
+                        'yes, of course',
+                    likelihoodscale: 8,
+                },
+                {
+                    response:
+                        'Sometimes',
+                    likelihoodscale: 4,
+                },
+            ],
+        },
+        {
+            question:
+                'Having people around me levels up my energy',
+            responses: [
+                {
+                    response: 'I like being alone, No',
+                    likelihoodscale: 3,
+                },
+                {
+                    response: 'Not so much',
+                    likelihoodscale: 4,
+                },
+                {
+                    response: 'Most definitely',
+                    elikelihoodscale: 8,
+                },
+                {
+                    response: 'Yes, I would say',
+                    likelihoodscale: 7,
+                },
+            ],
+        },
+        {
+            question: 'I am very thorough with my time, if wasted, I get angry',
+            responses: [
+                {
+                    response:
+                        'That is exactlt how I will feel',
+                    likelihoodscale: 3,
+                },
+                {
+                    response: 'Very accurate',
+                    likelihoodscale: 2,
+                },
+                {
+                    response: 'No, not me, I try to adjust my schedule',
+                    likelihoodscale: 8,
+                },
+                {
+                    response:
+                        'Not at all, wont be agry at such',
+                    likelihoodscale: 9,
+                },
+            ],
+        },
+    ];
+
+    return questions;
+}
