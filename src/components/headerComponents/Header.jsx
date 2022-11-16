8cd4b504f97b2c9cef529ffa8b83153748107152
iykeslim <iykeslim01@gmail.com>
2022-11-16 15:38:25 +0100

    first tested screen/component done and tests passed

--- src/components/headerComponents/Header.jsx
+++ src/components/headerComponents/Header.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { useNavigate } from 'react-router-dom';
 import teamway from './teamway.png';
 
@@ -11,9 +12,9 @@ const onClickRetake = () => {
   return (
     <div className='row'>
       <div className='col-12 my-3'>
-        <h4 className='text-center' onClick={onClickRetake} role='button'>Click to  Start Over</h4>
+        <h4 className='text-center' onClick={onClickRetake} role='button' data-testid='testForNavgtn'>Click to  Start Over</h4>
         <img src={teamway} className='d-block text-center mx-auto' style={{ width: '5rem' }} />
-        <h1 className='display-6 text-center'>Teamway Assessment Task</h1>
+        <h1 className='display-6 text-center' data-testid="welcome">Teamway Assessment Task</h1>
       </div>
     </div>
   )
