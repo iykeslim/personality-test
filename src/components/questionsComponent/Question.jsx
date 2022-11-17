a6f12102f3dbf1e868ef933c400e1b7ed15f9a91
iykeslim <iykeslim01@gmail.com>
2022-11-17 01:29:44 +0100

    questions component test and fixes as detected by test errors

--- src/components/questionsComponent/Question.jsx
+++ src/components/questionsComponent/Question.jsx
@@ -3,24 +3,27 @@ import Card from 'react-bootstrap/Card';
 import { useDispatch } from 'react-redux';
 import { addanswer } from '../../redux-files/actions/answer';
 
-const Question = ({ question, answers, selectanswer }) => {
+const Question = ({ question, answers, selectAnswer }) => {
   const dispatch = useDispatch()
 
 
   function onClickanswer(answer) {
-    selectanswer(answer);
+    selectAnswer(answer);
     dispatch(addanswer(answer))
   }
   return (
-    <div>
+    <div data-testid='question-show'>
       <Card className='shadow d-flex text-center'>
         <Card.Body>
           <div className=''>
             <Card.Header><h4>{question}</h4></Card.Header>
-            <ul className="list-group shadow-sm">
+            <ul className="list-group shadow-sm"
+              data-testid="answers-show"
+            >
               {answers.map((answer, key) => (
 
                 <li className="list-group-item"
+                  data-testid="answer-show"
                   role='button'
                   key={key}
                   onClick={() => onClickanswer(answer)}
---
+++ src/components/questionsComponent/Question.test.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import Question from './Question.jsx';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+const mockedDispatch = jest.fn();
+const clickedOn = jest.fn();
+const answers = [
+        {
+            answer: 'Yes, that is accurate for me',
+            likelihoodscale: 3,
+        },
+        {
+            answer:
+                'I really do not care as long as no one is bothering me',
+            likelihoodscale: 7,
+        },
+];
+
+jest.mock('react-redux', () => ({
+    useDispatch: () => mockedDispatch
+}));
+
+beforeEach(() => {
+    render(<Question answers={answers} selectAnswer={clickedOn} />);
+});
+
+test("renders question component", () => {
+    const qstnShow = screen.getByTestId('question-show');
+
+    expect(qstnShow).toBeInTheDocument();
+});
+
+describe('answers', () => {
+    
+            test("should be clickable", () => {
+                const answr = screen.getByText('Yes, that is accurate for me');
+                fireEvent.click(answr);
+        
+                expect(clickedOn).toHaveBeenCalled();
+            });
+    
+        test("should return the required data when clicked on", () => {
+            const clickShow = screen.getByText('Yes, that is accurate for me');
+            fireEvent.click(clickShow);
+    
+            expect(clickedOn).toHaveBeenCalledWith(answers[0]);
+        });
+
+        test("should display the answer", () => {
+            const answr = screen.getByText('I really do not care as long as no one is bothering me');
+            expect(answr).toBeInTheDocument();
+        });
+
+        test("should return all answers in the array", () => {
+            const answersElt = screen.getAllByTestId('answers-show');
+
+            expect(answersElt.length).toBe(1);
+            expect(answersElt).toHaveLength(1);
+
+            const answr = screen.getAllByTestId('answer-show');
+
+            expect(answr.length).toBe(2);
+            expect(answr).toHaveLength(2);
+        });
+
+});
