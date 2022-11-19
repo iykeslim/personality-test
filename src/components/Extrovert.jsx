ca9ed7d64bc1b6989a367ebeadee7f5b98aa81fa
iykeslim <iykeslim01@gmail.com>
2022-11-19 01:21:16 +0100

    quick fix, forgot a prop after restructuring

--- src/components/Extrovert.jsx
+++ src/components/Extrovert.jsx
@@ -4,7 +4,7 @@ import { Outcome } from './otcomeComponents/Outcome.jsx'
 
 export const Extrovert = () => {
   return (
-    <Outcome>
+    <Outcome result='EXTROVERT'>
         You are energized by and thrive off being around peopple: social activities bring out the best in you; you are more of an expressive person even when you are around strangers; you do well working in a group and you don't like being by yourself - that is when boredom steps in.
     </Outcome>
   )
