80b03c2bce628224bac764f6f995c99e63d2dc55
iykeslim <iykeslim01@gmail.com>
2024-03-27 02:14:10 +0100

    adding a loader for set timeout

--- src/components/questionsComponent/Question.jsx
+++ src/components/questionsComponent/Question.jsx
@@ -1,16 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import Card from "react-bootstrap/Card"
+import Spinner from "react-bootstrap/Spinner" // Import Spinner component
 import { useDispatch } from "react-redux"
 import { addAnswer } from "../../redux-files/actions/answer"
 import { Retake } from "../Retake.jsx"
 import "./Question.css" // Create a CSS file for styling
 
 const Question = ({ question, answers, selectAnswer }) => {
+  const [showSpinner, setShowSpinner] = useState(false) // State for controlling spinner visibility
   const dispatch = useDispatch()
 
-  function onClickanswer(answer) {
-    selectAnswer(answer)
-    dispatch(addAnswer(answer))
+  function onClickAnswer(answer) {
+    setShowSpinner(true) // Show spinner and overlay when transitioning to the next question
+    setTimeout(() => {
+      selectAnswer(answer)
+      dispatch(addAnswer(answer))
+      setShowSpinner(false) // Hide spinner and overlay after transitioning
+    }, 2000) // Adjust the timeout duration as needed (2000ms = 2 seconds)
   }
 
   // Function to toggle the alternate class for text color
@@ -22,11 +28,24 @@ const Question = ({ question, answers, selectAnswer }) => {
 
   return (
     <div data-testid="question-show" className="chat-container">
+      {showSpinner && ( // Render the overlay with spinner if showSpinner is true
+        <div className="overlay">
+          <Spinner animation="border" variant="primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      )}
       <Card className="d-flex text-center border-0 chat-bubble">
         <Card.Body>
           <div className="chat-content">
             <Card.Header className="border-0">
-              <h4 className="text-white border-0 chat-bubble-question">
+              <h4
+                className="text-white border-0 chat-bubble-question"
+                style={{
+                  fontSize: window.innerWidth > 768 ? "24px" : "18px",
+                  marginBottom: "5px",
+                }}
+              >
                 {question}
               </h4>
             </Card.Header>
@@ -39,8 +58,11 @@ const Question = ({ question, answers, selectAnswer }) => {
                   data-testid="answer-show"
                   role="button"
                   key={key}
-                  onClick={() => onClickanswer(answer)}
-                  style={{ fontSize: "1rem", marginBottom: "5px" }} // Adjust font size for smaller screens
+                  onClick={() => onClickAnswer(answer)}
+                  style={{
+                    fontSize: window.innerWidth > 768 ? "18px" : "14px",
+                    marginBottom: "5px",
+                  }} // Adjust font size for smaller screens
                 >
                   {answer.answer}
                 </li>
